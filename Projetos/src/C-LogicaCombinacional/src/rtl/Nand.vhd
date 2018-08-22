1c848610dde7cfedb5f4f5c87f39b1a4d48ed658
Rafael Corsi <corsiferrao@gmail.com>
2018-08-22 10:50:15 -0300

    add req. firebase + fix nand

--- Projetos/src/C-LogicaCombinacional/src/rtl/Nand.vhd
+++ Projetos/src/C-LogicaCombinacional/src/rtl/Nand.vhd
@@ -1,15 +1,15 @@
 Library ieee;
 use ieee.std_logic_1164.all;
 
-entity nand_z01 is
+entity nand_vhdl is
    port(
-          a : in  std_logic;
+        a : in  std_logic;
 	      b : in  std_logic;
 	      q : out std_logic
    );
 end entity;
 
-architecture rtl of nand_z01 is
+architecture rtl of nand_vhdl is
 begin
-   q <=  not (a and b);
+   q <=  a nand b;
 end rtl;
