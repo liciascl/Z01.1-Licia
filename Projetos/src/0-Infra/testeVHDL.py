a05f26320a93ced03ddc87704b580bd0866fe943
Rafael Corsi <rafael.corsi@insper.edu.br>
2018-12-20 09:53:42 -0200

    correcao notificao infra

--- Projetos/src/0-Infra/testeVHDL.py
+++ Projetos/src/0-Infra/testeVHDL.py
@@ -1,33 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-# Curso de Elementos de Sistemas
-# Desenvolvido por: Rafael Corsi <rafael.corsi@insper.edu.br>
-#
-# Adaptado de :     Pedro Cunial   <pedrocc4@al.insper.edu.br>
-#                   Luciano Soares <lpsoares@insper.edu.br>
-# Data de criação: 07/2017
-
 from os.path import join, dirname
-import sys, os, shutil, subprocess
+import sys, subprocess
 
-# Scripts python
 ROOT_PATH = subprocess.Popen(
     ['git', 'rev-parse', '--show-toplevel'],
     stdout=subprocess.PIPE).communicate()[0].rstrip().decode('utf-8')
-PROJ_PATH = os.path.join(ROOT_PATH, 'Projetos', 'src')
-TOOL_PATH = os.path.join(ROOT_PATH, 'Projetos', 'Z01-tools')
-TOOL_SCRIPT_PATH = os.path.join(TOOL_PATH, 'scripts')
-sys.path.insert(0, TOOL_SCRIPT_PATH)
-from report import report
-from notificacao import notificacao
-from testeVHDL import vhdlScript
+sys.path.insert(0, ROOT_PATH + '/Projetos/Z01-tools/scripts/')
+from config import *
 
 
 class tstInfra(object):
     def __init__(self):
         self.pwd = os.path.dirname(os.path.abspath(__file__))
-        self.log = os.path.join(TOOLSPATH,'log','logInfra.xml')
+        self.log = os.path.join(TOOL_PATH,'log','logInfra.xml')
         self.rtl = os.path.join(self.pwd, 'extras/testeVHDL/rtl/')
         self.tst = os.path.join(self.pwd, 'extras/testeVHDL/tst/')
         self.work = vhdlScript(self.log)
@@ -44,7 +30,7 @@ class tstInfra(object):
 
 
 if __name__ == "__main__":
-    n = notificacao()
+    n = notificacao('')
 
     # checa modelsim
     try:
@@ -62,7 +48,7 @@ if __name__ == "__main__":
     tstInfra.work.run()
 
     print("===================================================")
-    r = report(tstInfra.log, 'Infra')
+    r = report(tstInfra.log, 'Infra', None)
     error = r.hw()
     if error:
         n.error('Teste infra com problema')
